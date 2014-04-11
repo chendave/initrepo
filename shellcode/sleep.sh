8b9736e7968451c4621d3f865a76d62bbfc2b799
Dave Chen <wei.d.chen@intel.com>
2014-04-11 02:29:53 -0700

    hadoop 2.2.0 initialization Signed-off-by: Dave Chen <wei.d.chen@intel.com>

--- shellcode/sleep.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#the follow code canbe used to calculate the totoal time needed for some process run in backgroud
-sleep 2 &
-sleep 10 &
-sleep 5 &
-wait
--- shellcode/test.sh
+++
@@ -1,4 +0,0 @@
-#hello, dave
-#sort and omit repeated lines
-sort -n out.txt | uniq   > outupdate.txt
-
