d4135019c3fc699ab22c6b4915a623cc62908b9a
Dave Chen <wei.d.chen@intel.com>
2017-05-30 21:44:02 +0800

    Add the sample configuration for vagrant

---
+++ testcode/vagrant_start/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Use the default configuration provided here, you can
+# create a vagrant guest with a valid private IP address
+# and thus ssh to the guest and viceversa.
+sudo -E -H apt-get install -y virtualbox
+wget --no-check-certificate https://releases.hashicorp.com/vagrant/1.8.7/vagrant_1.8.7_x86_64.deb
+sudo dpkg -i vagrant_1.8.7_x86_64.deb
+vagrant init
+vagrant ssh control
+# You can find the shared directory "/vagrant/" here
