fec6c844f2a03e30f19752bf0e1b89c05fe338f2
Dave Chen <dave.jungler@gmail.com>
2019-04-28 15:00:15 +0800

    scripts for installl vnc and the configuration on linux

---
+++ vnc/config.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo apt-get install vnc4server -y
+sudo apt-get install xfce4-session
+cat <<EOF >/.vnc/xstartup
+#!/bin/sh
+
+# Uncomment the following two lines for normal desktop:
+# unset SESSION_MANAGER
+# exec /etc/X11/xinit/xinitrc
+
+[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
+startxfce4 &
+EOF
+
+
+# start vnc server
+vncserver -geometry 1440x900
