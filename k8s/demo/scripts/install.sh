2ecce1356c2d92298cc535199b5ce3dde99e68ff
Dave Chen <dave.jungler@gmail.com>
2019-11-29 17:00:27 +0800

    tidy up for the example configuration of k8s

---
+++ k8s/demo/scripts/install.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+sudo swapoff -a
+
+# install docker
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+
+# on arm
+sudo add-apt-repository \
+   "deb [arch=arm64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+# do a sanity check
+sudo docker run hello-world
+
+# install kubeadm/kubectl
+apt-get update && apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
+
+# setup cluster with kubeadm
+
+# replace "10.169.40.58" with your host IP
+kubeadm init --apiserver-advertise-address 10.169.40.58 --pod-network-cidr=10.244.0.0/16
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/62e44c867a2846fefb68bd5f178daf4da3095ccb/Documentation/kube-flannel.yml
+
+# make it work for non-root user, might need to remove the cofig file
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+
+# join other nodes
+#kubeadm join xxx --token xxx --discovery-token-ca-cert-hash xxx
