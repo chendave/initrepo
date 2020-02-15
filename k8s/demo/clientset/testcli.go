da94cbae12a8057d2676716aed1abad802c49376
Dave Chen <dave.jungler@gmail.com>
2020-02-15 16:25:11 +0800

    update the example on the clientset

--- k8s/demo/clientset/testcli.go
+++ k8s/demo/clientset/testcli.go
@@ -1,15 +1,19 @@
-//https://uzshare.com/view/793594
+//credit to: //https://uzshare.com/view/793594
+//based on commit id: a67238eed67315169b4ff97c73860002c39ea428
+//both dynamic and clientset is based on restclient
+//dynamic is unstructured, clientset and restclient is structured.
+//for the clientset, see the implmentation here: k8s.io/client-go/kubernetes/typed/core/v1/pod.go:116
+//run in this way: go run testcli.go  --kubeconfig=/root/.kube/config
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
-	"context"
 	"k8s.io/api/core/v1"
 	v12 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	//"k8s.io/apimachinery/pkg/runtime/serializer"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
@@ -22,20 +26,18 @@ var (
 	kubeconfigStr = flag.String("kubeconfig", "default value", "kubernetes config file")
 )
 
-func main()  {
+func main() {
 	//解析参数
 	flag.Parse()
 
 	testClientSet()
 
 	fmt.Println("\nrest")
-
 	testRestClient()
 
 	fmt.Println("\n.....")
 	testDynamicClient()
 }
-
 
 func testRestClient() {
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfigStr)
@@ -47,13 +49,13 @@ func testRestClient() {
 	config.APIPath = "/api"
 	//pods资源相关的group为空
 	config.GroupVersion = &schema.GroupVersion{
-		Group:    "",
+		Group:   "",
 		Version: "v1",
 	}
 	//序列化方式，目前json和protocal buf
 	config.ContentType = runtime.ContentTypeJSON
-	//config.NegotiatedSerializer = serializer.DirectCodecFactory{CodecFactory: scheme.Codecs}
 	config.NegotiatedSerializer = scheme.Codecs.WithoutConversion()
+	config.UserAgent = rest.DefaultKubernetesUserAgent()
 
 	restClient, err := rest.RESTClientFor(config)
 	if err != nil {
@@ -62,11 +64,12 @@ func testRestClient() {
 
 	podList := &v1.PodList{}
 	//除了Do()方法之外，还有DoRaw()，返回原始的bytes； Do()会做一下类型的转化
-	restClient.Get().Resource("pods").Namespace("").Do(context.TODO()).Into(podList)
+	restClient.Get().Resource("pods").Namespace("default").Do(context.TODO()).Into(podList)
 
-	fmt.Println(podList)
+	fmt.Println(len(podList.Items))
+	fmt.Println("-------------------------")
+	fmt.Println(podList.Items)
 }
-
 
 func testClientSet() {
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfigStr)
@@ -80,12 +83,11 @@ func testClientSet() {
 	}
 
 	//一行代码指定group、version、resource、以及动作
-	//podList, err := clientset.CoreV1().Pods("kube-system").List(v12.ListOptions{})
 	//podInfo, err := clientset.CoreV1().Pods("kube-system").Get("etcd-node-master", v12.GetOptions{})
-	//podList, err := clientset.CoreV1().Pods("").List(v12.ListOptions{})
-	//podList, err := clientset.CoreV1().Pods("default").List(v12.ListOptions{})
-	fmt.Println("below is the pod info")
-	fmt.Println(podInfo)
+	podList, err := clientset.CoreV1().Pods("default").List(v12.ListOptions{})
+	fmt.Println(len(podList.Items))
+	fmt.Println("-------------------------")
+	fmt.Println(podList.Items)
 }
 
 func testDynamicClient() {
@@ -99,14 +101,14 @@ func testDynamicClient() {
 		panic(err)
 	}
 
-	//指定group、version以及要访问的资源 
+	//指定group、version以及要访问的资源
 	testrcGVR := schema.GroupVersionResource{
 		Group:    "",
 		Version:  "v1",
 		Resource: "pods",
 	}
 
-	unstr, err := dynamicClient.Resource(testrcGVR).List(v12.ListOptions{})
+	unstr, err := dynamicClient.Resource(testrcGVR).Namespace("default").List(v12.ListOptions{})
 
 	fmt.Println(unstr)
 }
