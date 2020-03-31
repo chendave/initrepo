98860b7c7c3934802d57909f84db6fa8bfe7a946
Dave Chen <dave.jungler@gmail.com>
2020-03-31 12:12:38 +0800

    update example on the http listen

--- go/httpListen/listen.go
+++ go/httpListen/listen.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -15,13 +16,16 @@ func main() {
 		io.WriteString(w, "Hello, world!\n")
 	}
 
-	log.Printf("Before the listen ...Go to https://127.0.0.1:8443/")
+	log.Printf("Before the listen ...Go to http://127.0.0.1:8181/hello")
 	http.HandleFunc("/hello", helloHandler)
 	//run forever with the below statement.
-	//log.Fatal(http.ListenAndServe(":8080", nil))
+	//log.Fatal(http.ListenAndServe(":8181", nil))
 	//exit imediately after the go routine.
 	go func() {
-		log.Fatal(http.ListenAndServe(":8080", nil))
+		log.Printf("in the goroutine...Go to http://127.0.0.1:8181/hello")
+		log.Fatal(http.ListenAndServe(":8181", nil))
 	}()
-	log.Printf("Hit after the listen ...Go to https://127.0.0.1:8443/")
+	log.Printf("Hit after the listen ...Go to http://127.0.0.1:8181/hello")
+	time.Sleep(20 * time.Second)
+
 }
